15593f036e34de0283e27da7b86cffe5d9dc725c
orion <munna.csehstu@gmail.com>
2024-10-03 16:04:45 +0600

    completed header section and started main section

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,23 @@
 console.log("script connected");
+
+// fetching categories data
+fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(response => response.json())
+    .then(data => loadCategories(data.categories));
+
+// loading categories data
+function loadCategories(categories) {
+    categories.forEach((category) => {
+        // console.log(category);
+        displayCategories(category);
+    })
+}
+
+function displayCategories(category) {
+    const categoriesContainer = document.getElementById('categories-container');
+    console.log(category);
+    const categoryBtn=document.createElement('button');
+    categoryBtn.innerText=category.category;
+    categoryBtn.classList="btn font-bold"
+    categoriesContainer.appendChild(categoryBtn);
+}
